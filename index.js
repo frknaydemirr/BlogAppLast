37887330fb5d887d69753a7490bca81e0957a956
frknaydemirr <furkanaydemir12@hotmail.com>
2026-02-12 15:50:13 +0300

    model structure entegrated

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
 
+const Blog = require("./models/blog");
+const Category = require("./models/category");
+
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
